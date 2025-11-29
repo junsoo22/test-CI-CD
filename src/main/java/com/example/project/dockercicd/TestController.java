619b017b4a253900d6fda66ac7b2a45f53b900be
kim junsoo <kjs201105@naver.com>
2025-11-29 15:17:31 +0900

    test용

--- src/main/java/com/example/project/dockercicd/TestController.java
+++ src/main/java/com/example/project/dockercicd/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    private static final String version="v1.0.3";
+    private static final String version="v1.0.4";
 
     @GetMapping("/test")
     public String home() {
